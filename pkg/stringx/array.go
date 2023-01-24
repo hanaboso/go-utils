92115317024bcfaa9aa5fabb9121337a61a3bcff
David Horacek <horacek.d@hanaboso.com>
2023-01-24 09:51:43 +0100

    stringx.FromArray

--- pkg/stringx/array.go
+++ pkg/stringx/array.go
@@ -9,3 +9,19 @@ func InArray(haystack []string, needle string) bool {
 
 	return false
 }
+
+func FromArray(haystack []string, index int) string {
+	if len(haystack) > index {
+		return haystack[index]
+	}
+
+	return ""
+}
+
+func FromArrayDefault(haystack []string, index int, defaultValue string) string {
+	if len(haystack) > index {
+		return haystack[index]
+	}
+
+	return defaultValue
+}
