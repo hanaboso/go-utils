a75fe678e4f82e0200c55a06d597ae17c841d6fc
David Horacek <horacek.d@hanaboso.com>
2023-01-27 12:25:23 +0100

    contextx utils

--- pkg/contextx/context.go
+++ pkg/contextx/context.go
@@ -9,6 +9,16 @@ func WithTimeout(timeout time.Duration) (context.Context, context.CancelFunc) {
 	return context.WithTimeout(context.Background(), timeout)
 }
 
+func WithTimeoutCtx(timeout time.Duration) context.Context {
+	ctx, _ := context.WithTimeout(context.Background(), timeout)
+	return ctx
+}
+
 func WithTimeoutSeconds(seconds int) (context.Context, context.CancelFunc) {
 	return context.WithTimeout(context.Background(), time.Duration(seconds)*time.Second)
+}
+
+func WithTimeoutSecondsCtx(seconds int) context.Context {
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(seconds)*time.Second)
+	return ctx
 }
